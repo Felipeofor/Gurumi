e8bdcb53a45e6464077007e5d8643bdfe76b939b
Alejo Cecco <acecco6@gmail.com>
2021-07-22 19:11:28 -0300

    responsive menu

--- Assets/js/nav.js
+++ Assets/js/nav.js
@@ -3,3 +3,9 @@ $("#down").on("click", function() {
     $("#down__option").toggle(duration=200);
   })
 
+$("#header__button__active").on("click",function() {
+  $("#header__menu").fadeIn(duration=200);
+})
+$("#header__button__close").on("click",function() {
+  $("#header__menu").fadeOut(duration=200);
+})
