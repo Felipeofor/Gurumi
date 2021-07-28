90939612ed51e3a6a3437ee6cc968ae65584c48a
Alejo Cecco <acecco6@gmail.com>
2021-07-28 01:51:43 -0300

    prueba modal movil

--- Assets/js/modal.js
+++ Assets/js/modal.js
@@ -1,8 +1,24 @@
 
-$(document).ready(function(){
-    $(".card").each(function(index,element){
-        $(element).on("click",function(){
-            $("#modal").css("display", "flex");
+
+$.getJSON( "../Assets/js/productos.json", function(data){
+    let card__content=document.getElementById("card__content")
+    for (const producto of data) {
+        card__content.innerHTML+=`
+            <div class="card" onclick="modal()">
+                <div class="card__img">
+                    <img src="${producto.img[0]}">
+                </div>
+                <div class="card__text">
+                    <h3 class="">${producto.nombre}</h3>
+                    <p class="">Medidas ${producto.medidas}</p>
+                </div>
+            </div>`
+    }
+   
+}) 
+
+function modal() {
+    $("#modal").css("display", "flex");
             $("#modal").css("position", "fixed");
         
         setTimeout(() => {
@@ -14,35 +30,16 @@ $(document).ready(function(){
                     imagen.setAttribute("src", direccion);
                 })
             }
-        },10);
-        })
-    });
-    $("#close__modal").on("click",function(){
+        },10)
+}
+
+$("#close__modal").on("click",function(){
     
-        $("#modal").css("display", "none")
-        $("#modal__content").css("transform","translateY(-200%)")
-    })
-    
+    $("#modal").css("display", "none")
+    $("#modal__content").css("transform","translateY(-200%)")
 })
 
 
 
 
-
-// $("#card").on("click",function(){
-
-//     $("#modal").css("display", "flex");
-//     $("#modal").css("position", "fixed");
-    
-//     setTimeout(() => {
-//         $("#modal__content").css("transform","translateY(0%)")
-//         for (const img of $(".img__modal__option")) {
-//             img.addEventListener("click",function(){
-//                 let direccion=img.getAttribute("src")
-//                 let imagen=document.getElementById("img__modal")
-//                 imagen.setAttribute("src", direccion);
-//             })
-//         }
-//     },100);
-// })
 
