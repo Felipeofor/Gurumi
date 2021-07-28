856eb9bf33db56f0dd59106d54772fb4cc9a2633
Alejo Cecco <acecco6@gmail.com>
2021-07-28 00:34:19 -0300

    amigurumis / Modal / Responsive

---
+++ Assets/js/modal.js
@@ -0,0 +1,45 @@
+
+
+$(".card").each(function(index,element){
+    $(element).on("click",function(){
+        $("#modal").css("display", "flex");
+        $("#modal").css("position", "fixed");
+    
+    setTimeout(() => {
+        $("#modal__content").css("transform","translateY(0%)")
+        for (const img of $(".img__modal__option")) {
+            img.addEventListener("click",function(){
+                let direccion=img.getAttribute("src")
+                let imagen=document.getElementById("img__modal")
+                imagen.setAttribute("src", direccion);
+            })
+        }
+    },10);
+    })
+});
+
+
+
+
+// $("#card").on("click",function(){
+
+//     $("#modal").css("display", "flex");
+//     $("#modal").css("position", "fixed");
+    
+//     setTimeout(() => {
+//         $("#modal__content").css("transform","translateY(0%)")
+//         for (const img of $(".img__modal__option")) {
+//             img.addEventListener("click",function(){
+//                 let direccion=img.getAttribute("src")
+//                 let imagen=document.getElementById("img__modal")
+//                 imagen.setAttribute("src", direccion);
+//             })
+//         }
+//     },100);
+// })
+
+$("#close__modal").on("click",function(){
+    
+    $("#modal").css("display", "none")
+    $("#modal__content").css("transform","translateY(-200%)")
+})
